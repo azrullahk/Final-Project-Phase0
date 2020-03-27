16c1a880d0f3eec8437133009eab1e65eec151f7
azrullahk <azrullahkainage@gmail.com>
2020-03-27 16:06:31 +0700

    revert kartu tidak hilang

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function reset (angka) {
 }
 
 function disable (angka) {
-    angka.style.display='none'
+    // angka.style.display='none'
     angka.style.backgroundColor='cyan';
     angka.style.width= '105px';
     angka.style.height= '105px';
